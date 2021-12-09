a711e40e46162b9b705933e7df2f8afe2df093af
Sergio Soto <me@srgio.es>
2021-12-09 21:13:26 +0100

    chore: Remove image requirements

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -51,7 +51,7 @@ const mainBody = {
 const about = {
   show: false,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  // imageLink: require("../editable-stuff/hashirshoaeb.png"),
   imageSize: 375,
   message:
     "Dolor sit amet",
@@ -79,12 +79,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      // img: require("../editable-stuff/hashirshoaeb.png"),
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      // img: require("../editable-stuff/hashirshoaeb.png"),
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
